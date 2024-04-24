1d2e691b691b89e8a56e0635637987d8b13740e1
Anshuman Dey Kirty <anshuman.kirty@gmail.com>
2024-04-24 11:52:19 -0400

    Adding Requirements.txt

--- pipeline/pdf_ocr.py
+++ pipeline/pdf_ocr.py
@@ -1,9 +1,7 @@
 import subprocess
 
-# Combine command and arguments
 cmd = "surya_ocr data/sample2.pdf --langs en --results_dir data/results/"
 
-# Execute the command
 try:
     result = subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True)
     print("Command executed successfully:")
